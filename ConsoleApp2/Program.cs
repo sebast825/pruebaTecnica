b9ac69dbea2efc9cdcbd8b3440446cbbe6897a1c
Sebastian Molina <83253419+sebast825@users.noreply.github.com>
2026-09-07 18:20:04 -0300

    first comit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+
+
+var stringConnection = "Server=localhost ,1433; Database=DB_NAME; User Id=sa; Password=PASSWORD; TrustServerCertificate=True;";
+
+var connection = new SqlConnection(stringConnection);
+await connection.OpenAsync();
+
+var command = new SqlCommand("getOrderByiD ", connection);
+
+command.CommandType = CommandType.StoredProcedure;
+
+try
+{
+    Console.Write("Ingrese el número de orden: ");
+
+    if (!int.TryParse(Console.ReadLine(), out int orderId))
+    {
+        Console.WriteLine("El número de orden no es válido.");
+        return;
+    }
+
+    command.Parameters.AddWithValue("@id", orderId);
+
+
+    var reader = await command.ExecuteReaderAsync();
+
+    Console.WriteLine($"Orden Consultada: {orderId}");
+    Console.WriteLine();
+    if (await reader.ReadAsync())
+    {
+        ShowCompanyDetails(reader);
+
+    }
+    Console.WriteLine();
+
+    if (await reader.NextResultAsync())
+    {
+        await ShowOrderProductsDetails(reader);
+    }
+
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
+
+
+
+static void ShowCompanyDetails(SqlDataReader reader)
+{
+    Console.WriteLine($"Empresa: {reader["CompanyName"]}");
+    Console.WriteLine($"Dirección: {reader["Address"]}");
+}
+
+static async Task ShowOrderProductsDetails(SqlDataReader reader)
+{
+    Console.WriteLine(
+    $"{"Producto",-30} {"Precio",10} {"Cantidad",10} {"Otras órdenes",15}"
+);
+
+    Console.WriteLine(new string('-', 70));
+
+    while (await reader.ReadAsync())
+    {
+        Console.WriteLine(
+            $"{reader["ProductName"],-30} " +
+            $"{reader["UnitPrice"],10} " +
+            $"{reader["Quantity"],10} " +
+            $"{reader["CountProductoEnOrdenesDistitnas"],15}"
+        );
+    }
+}
---
+++ storeProcedure.sql
@@ -0,0 +1,33 @@
+alter procedure getOrderByiD
+@id int
+
+AS
+BEGIN
+	IF NOT EXISTS (SELECT * FROM Orders WHERE OrderID = @id)
+	 
+	BEGIN
+	RAISERROR('La orden no existe ', 16,1)
+	return
+	END
+	ELSE 
+	BEGIN
+	PRINT 'La orden existe'
+	end
+
+		select CompanyName, Address from customers
+	inner join Orders
+	on orders.CustomerID = Customers.CustomerID
+	where orders.OrderID = @id
+
+	select Products.ProductName, Products.UnitPrice, od.Quantity , (select count(*) from [Order Details] where   orderId != @id and  ProductID = Products.ProductID  ) as CountProductoEnOrdenesDistitnas from [Order Details] as od
+	inner join orders
+	on orders.OrderID = od.OrderID
+	inner join Products 
+	on od.ProductID = Products.ProductID	
+	group by Products.ProductID,Products.ProductName, Products.UnitPrice,orders.OrderID,  od.Quantity
+	having orders.OrderID = @id
+
+END
+
+
+
